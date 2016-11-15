72fc01c1eefc705c047bb08d5f03e091a2e0a17f
Diego Calle <dnetix@gmail.com>
2016-11-15 17:57:24 -0500

    Updating the redirection library

--- lib/Dnetix/redirection/src/Carrier/SoapCarrier.php
+++ lib/Dnetix/redirection/src/Carrier/SoapCarrier.php
@@ -21,7 +21,7 @@ class SoapCarrier extends Carrier
         $config = array_merge([
             'soap_version' => SOAP_1_2,
             'features' => SOAP_SINGLE_ELEMENT_ARRAYS,
-            'cache_wsdl' => WSDL_CACHE_BOTH,
+            'cache_wsdl' => WSDL_CACHE_DISK,
             'trace' => false,
             'encoding' => 'UTF-8',
         ], $config);
